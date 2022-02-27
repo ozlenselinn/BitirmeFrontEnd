32b75c66ec3ca484ad2028df6ba1a72c1cd2454d
ozlenselinn <ozlen_selin@hotmail.com>
2022-02-27 17:09:17 +0300

    Login page-fe- yapıldı

--- src/index.js
+++ src/index.js
@@ -1,7 +1,17 @@
-const ReactParticles = require("./cjs/particles");
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import "./index.css";
 
-for (let key in ReactParticles) {
-    ReactParticles.default[key] = ReactParticles[key];
-}
+ReactDOM.render(
+  <React.StrictMode>
+    <App />
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
-module.exports = ReactParticles.default;
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
