c16da9035292dcd9f39251ef0c6d86035594e86a
ozlenselinn <ozlen_selin@hotmail.com>
2022-03-22 23:35:53 +0300

    addWantedPeople form

--- src/App.js
+++ src/App.js
@@ -3,6 +3,8 @@ import {BrowserRouter as Router,Switch, Route} from "react-router-dom";
 import Login from "./Components/Pages/LoginPage/Login";
 import Navbar from "./Components/Pages/HomePage/Navbar";
 import Home from "./Components/Pages/HomePage/Home";
+import AddWantedPerson from "./Components/Pages/AddEntity/AddWantedPerson";
+
 import './App.css';
 
 function App(Component) {
@@ -17,6 +19,8 @@ function App(Component) {
     
     <Route path="/" exact component={Login}/>
     <Route path="/Home" component={Home}/>
+    <Route path="/AddWantedPerson" component={AddWantedPerson}/>
+
  
       
     </Switch>
