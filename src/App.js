e80097961414a2d4e44ab9fbba1c49181070eb8b
ozlenselinn <ozlen_selin@hotmail.com>
2022-03-23 11:50:37 +0300

    axios

--- src/App.js
+++ src/App.js
@@ -4,8 +4,11 @@ import Login from "./Components/Pages/LoginPage/Login";
 import Navbar from "./Components/Pages/HomePage/Navbar";
 import Home from "./Components/Pages/HomePage/Home";
 import AddWantedPerson from "./Components/Pages/AddEntity/AddWantedPerson";
-
+import ListEmployeeComponent from "./Components/Pages/Tutorial/ListEmployeeComponent";
+import HeaderComponent from "./Components/HeaderComponent";
+import FooterComponent from "./Components/FooterComponent";
 import './App.css';
+
 
 function App(Component) {
  
@@ -20,6 +23,12 @@ function App(Component) {
     <Route path="/" exact component={Login}/>
     <Route path="/Home" component={Home}/>
     <Route path="/AddWantedPerson" component={AddWantedPerson}/>
+    
+    <div className="container">
+    <HeaderComponent></HeaderComponent>
+    <Route path="/Tutorial" component={ListEmployeeComponent}/>
+    <FooterComponent></FooterComponent>
+    </div>
 
  
       
